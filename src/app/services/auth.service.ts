f5603fb2a48ebb9abaf7e343c8af2053903c79e5
algassby <algassby@hotmail.com>
2020-11-29 21:16:54 +0100

    generation du formulaire signupcomponent

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase';
+import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,40 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+/**
+ * Creation d'un user d'authentfication
+ * @param email 
+ * @param password 
+ */
+  createNewUser(email:string, password:string){
+    return new Promise(
+      (resolve, reject)=>{
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          ()=>{
+            resolve();
+          },
+          (error)=>{
+            reject(error);
+          }
+        );
+      }
+    );
+
+  }
+
+  /**
+   * gere la connexion connecter
+   * @param email 
+   * @param password 
+   */
+  signin(email:string, password:string){
+    return new Promise(
+      (resolve, reject)=>{
+
+        firebase.auth().signInWithEmailAndPassword(email, password);
+
+      }
+
+    );
+  }
 }
